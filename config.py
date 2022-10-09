6d6fe56c01cbbb141c6d5a2fe16b2caad94d34bd
volodymyr.momot <volodymyr.momot@itomy.ch>
2022-10-10 00:20:02 +0300

    add dotenv

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 
 from utils import FileSystem, logger
 
-dotenv_path = FileSystem.get_absolute_path('.env')
+dotenv_path: str = FileSystem.get_absolute_path('.env')
 if os.path.exists(dotenv_path):
     logger.info(f'Load env vars from {dotenv_path}')
     load_dotenv(dotenv_path)
 
 
 class Config(NamedTuple):
-    base_url: str = os.environ.get("BASE_URL", "http://localhost:8000")
+    base_url: str = os.environ.get("BASE_URL", "http://localhost:8080")
