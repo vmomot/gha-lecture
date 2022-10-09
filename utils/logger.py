29e2aebde6ee614ec8c3ad22af74b194ba5f792c
volodymyr.momot <volodymyr.momot@itomy.ch>
2022-10-10 00:20:30 +0300

    add add hinting

--- utils/logger.py
+++ utils/logger.py
@@ -5,6 +5,6 @@ from utils import FileSystem
 from loguru import logger
 
 
-logs_folder_path = FileSystem.get_absolute_path("logs")
-log_name = f'{datetime.datetime.now().strftime("%d-%m_%H_%M")}.log'
+logs_folder_path: str = FileSystem.get_absolute_path("logs")
+log_name: str = f'{datetime.datetime.now().strftime("%d-%m_%H_%M")}.log'
 logger.add(os.path.join(logs_folder_path, log_name), rotation="500 MB")
