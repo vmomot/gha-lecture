282d6d6369159482638fdae6e99316431b9dc836
volodymyr.momot <volodymyr.momot@itomy.ch>
2022-10-28 13:23:50 +0300

    delete unused fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,13 +10,6 @@ def config() -> Config:
     return config
 
 
-# @fixture
-# def customer(config, context) -> CustomerActor:
-#     customer = CustomerActor(base_url=config.base_url,
-#                              browser_context=context)
-#     return customer
-
-
 @fixture(scope='function')
 def shop_page(config, page) -> ShopPage:
     page = ShopPage(base_url=config.base_url, playwright_page=page)
